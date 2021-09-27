c6a68c7e158246a6c65ee0f4c85a3620b958c513
haxyjp <85060163+haxyjp@users.noreply.github.com>
2021-09-27 09:41:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+var_dump($_POST);
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -275,6 +278,17 @@ article.reply::before {
 <body>
 <h1>ひと言掲示板</h1>
 <!-- ここにメッセージの入力フォームを設置 -->
+	<form method="post">
+	<div>
+		<label for="view_name">表示名</label>
+		<input id="view_name" type="text" name="view_name" value="">
+	</div>
+	<div>
+		<label for="message">ひと言メッセージ</label>
+		<textarea id="message" name="message"></textarea>
+	</div>
+	<input type="submit" name="btn_submit" value="書き込む">
+</form>
 <hr>
 <section>
 <!-- ここに投稿されたメッセージを表示 -->
